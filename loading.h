f96fb1658d70f06dc1044b9ee352e7940cb016a7
BilalELOTMANI <bilalelotmani100@gmail.com>
2024-08-09 20:03:25 +0200

    Function modified

--- loading.h
+++ loading.h
@@ -1,30 +1,47 @@
-#include<iostream>
-#include<windows.h>
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
 
 void loadingBar()
 {
-	system("cls");
-	system("color 9F");
+    system("cls");
+    system("color 1F");
 
-	char a = 177, b = 219;
-	//cout<<"\n\n\n\n\n\t\t\t\t\tStudent's Data Management System";
-	cout<<"\n\t\t\t\t\tProject By Ajay Singh";
-	cout<<"\n\n\n\t\t\t\t\tLoading...\n";
-	cout<<endl;
-	cout<<"\t\t\t\t\t";
-	
-	for(int i = 0;i<26;i++)
+    // Fetch console width dynamically
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
 
-		cout << a;
+    int barLength = columns / 2; // Loading bar takes half of the screen width
+    int loadingPosition = (columns - barLength) / 2; // Center loading bar
 
-	cout << "\r";
-	cout << "\t\t\t\t\t";
-	for (int i = 0; i < 26; i++)
-	{
-		cout << b;
-		Sleep(200);
-	}
+    char a = 177, b = 219;
+
+    cout << "\n\n\n\n";
+    cout << string((columns - 38) / 2, ' ') << "Student's Records Management System";
+    cout << "\n";
+    cout << string((columns - 22) / 2, ' ') << "Project By Bilal EL-OTMANI";
+    cout << "\n\n\n";
+    cout << string((columns - 10) / 2, ' ') << "Loading...\n";
+    cout << "\n";
+    cout << string(loadingPosition, ' ');
+
+    // Print initial loading bar
+    for (int i = 0; i < barLength; i++) cout << a;
+
+    cout << "\r";
+    cout << string(loadingPosition, ' ');
+
+    // Progressively fill the loading bar
+    for (int i = 0; i < barLength; i++)
+    {
+        cout << b;
+        Sleep(100);
+    }
+
+    cout << "\n\n";
 }
 
